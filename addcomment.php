80e33eff9dc67979ffceb451af82d71d886d53c9
choyukchow <choyukchow@gmail.com>
2013-12-23 23:09:18 +0800

    finish comment

--- addcomment.php
+++ addcomment.php
@@ -1,12 +1,21 @@
-<?php
-    session_start();
-    $comment = $_POST['comment'];
-    $blog_id = $_POST['blog_id'];
-    $link = mysqli_connect("localhost", "root", "jc119@3fcmx", "assignment1");
+<html>
+<head>
+	<title> Successful comment! </title>
+</head>
 
-    $query = "INSERT INTO comment (Blog_ID, date, username, comment)
+<body>
+  <h2> Successful comment! </h2>
+
+  <?php
+      session_start();
+      $comment = $_POST['comment'];
+      $blog_id = $_POST['blog_id'];
+      $link = mysqli_connect("localhost", "root", "jc119@3fcmx", "assignment1");
+
+      $query = "INSERT INTO comment (Blog_ID, date, username, comment)
               VALUES (".$blog_id.", now(), '".$_SESSION['valid_user']."', '".$comment."')";
-    mysqli_query($link, $query);
+      mysqli_query($link, $query);
 
-    header("Location:showpage.php");     
-?>
+      echo "<a href = 'showpage.php?blog_id=".$blog_id."'> Back to blog</a><br>";
+  ?>
+</body>
--- showpage.php
+++ showpage.php
@@ -1,6 +1,11 @@
 <html>
 <head>
   <title> Show page. </title>
+  <style>
+    .blogcontent{
+        width: 1200px;
+       }
+  </style>
 </head>
 
 <body>
@@ -17,26 +22,25 @@
     echo "<h1>".$result['title']."</h1><br>";
     echo $result['username'];
     echo $result['date']."<br>";
-    echo $result['content']."<br>";  
+    echo "<div class='blogcontent'>".nl2br($result['content'])."<br></div>";  
 
     if ($result['username'] === $_SESSION['valid_user']) {
-      echo "<a href='delete.php?blog_id=$blog_id'> Delete </a> | ";
-
-      echo "<a href='editpage.php?blog_id=$blog_id'> Edit </a>";
+      echo "<a href='delete.php?blog_id=$blog_id'> Delete </a> | <a href='editpage.php?blog_id=$blog_id'> Edit </a>";
     }
 
     echo "<form action = 'default.php' method = 'post'>
             <p> <input type = 'submit' value = 'Back to catalog'> </p>
           </form>";
 
-    //comment function
+    //comment
     echo "<form action = 'addcomment.php' method = 'post'>
              <h6> My Comment </h6>
              <p> <textarea rows='5' cols='50' name = 'comment'>My comment... </textarea> </p>
-             <p> <input type = 'hidden' name = 'blog_id' value = '.$blog_id.'> </p>
+             <p> <input type = 'hidden' name = 'blog_id' value = '".$blog_id."'> </p>
              <p> <input type = 'submit' value = 'Comment'> </p>
           </form>";
-
+    
+    //show comment
     $query = "SELECT * FROM comment WHERE Blog_ID = '".$blog_id."'";
     $result = mysqli_query($link, $query);
     $result_rows = mysqli_num_rows($result);
@@ -48,7 +52,7 @@
     while ($result_rows > 0) {
       $current_comment = mysqli_fetch_assoc($result);
       echo $current_comment['date']."|".$current_comment['username']."  ".$current_comment['comment']."<br>";
-      $rows = $rows - 1;
+      $result_rows = $result_rows - 1;
     }
   ?>
 </body>
