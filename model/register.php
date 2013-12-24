ecbd3a9f4b8803a7936511a0bb58e9ca207fddaa
Pan Long <aga.panlong@gmail.com>
2013-12-25 04:25:48 +0800

    if the user name exists, do not proceed to register

--- model/register.php
+++ model/register.php
@@ -1,6 +1,6 @@
 <?php    
     include $_SERVER['DOCUMENT_ROOT'].'/assignment1/config/global.php';
-    $username = $_POST['username'];
+    $username = trim($_POST['username']);
     $password = $_POST['password'];
     $link = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 
@@ -10,7 +10,10 @@
     } else {
         $query = "INSERT INTO passwords (username, password)
             VALUES ('".$username."', '".$password."')";
-        mysqli_query($link, $query);
+        if (mysqli_query($link, $query)){
+            session_start();
+            $_SESSION['valid_user'] = $username;
+        }
 
         header("Location:".VIEW_DIR."/default.php");
     }
